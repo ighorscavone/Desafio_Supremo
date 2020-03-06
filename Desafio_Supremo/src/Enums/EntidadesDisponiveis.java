c5d7b356137a3824e490ca19ba266a8377e4c234
IGHOR IBRAIM SCAVONE <082170011@ftt.cefsa.edu.br>
2020-03-05 22:42:21 -0300

    Adição de coisas

---
+++ Desafio_Supremo/src/Enums/EntidadesDisponiveis.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+/**
+ *
+ * @author 082170011
+ */
+public enum EntidadesDisponiveis {
+    
+    USUARIO("usuario"),
+    PRODUTO("produto"),
+    PEDIDO("pedido"),
+    FUNCIONARIO("funcionario"),
+    CLIENTE("cliente");
+    
+    private String descricao;
+    
+    EntidadesDisponiveis(String descricao){
+        this.descricao = descricao;
+    }
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    
+}
